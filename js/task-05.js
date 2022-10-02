4071ee41b9d0f34fca52be06f0bec92da0b0680b
Anton Kovsh <kovsh_anton@ukr.net>
2022-10-02 18:28:59 +0300

    8 done! 9 and 10 prepared

--- js/task-05.js
+++ js/task-05.js
@@ -5,51 +5,18 @@
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
-// const inputRef = document.querySelector("#name-input");
-// const outputRef = document.querySelector("#name-output");
+const inputRef = document.querySelector("#name-input");
+const outputRef = document.querySelector("#name-output");
 
-// inputRef.addEventListener("input", onInputChange);
-
-// function onInputChange(event) {
-
-// }
-
-//   if (inputRef.textContent === "") {
-//     outputRef.textContent = Anonymous;
-//   }
-//   if (inputRef.textContent !== "") {
-//     outputRef.textContent = event.currentTarget.value;
-//   }
-
-// function onInputChange(event) {
-//   console.log(outputRef.textContent.value);
-
-//   if (inputRef.textContent.value !== "") {
-//     outputRef.textContent = event.currentTarget.value;
-//   }
-//   if (inputRef.textContent.value === "") {
-//     outputRef.textContent = "Anonymous";
-//   }
-// }
-
-//  console.log(event.currentTarget.value);
-//  outputRef.textContent = event.currentTarget.value;
-
-const refs = {
-  input: document.querySelector("#name-input"),
-  output: document.querySelector("#name-output"),
-};
-
-console.log(refs.input.textcontent);
-
-refs.input.addEventListener("input", onInputChange);
+inputRef.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
+  // console.log(event.currentTarget.value);
+  console.log(outputRef.textContent);
   console.log(event.currentTarget.value);
-  if (refs.input.textContent.value === "") {
-    refs.output.textContent = "Anonymous";
-  }
-  if (refs.input.textContent.value !== "") {
-    refs.output.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    outputRef.textContent = "Anonymous";
+  } else {
+    outputRef.textContent = event.currentTarget.value;
   }
 }
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,58 @@
+// Напиши скрипт управления формой логина.
 
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Login</button>
+// </form>
+
+//     Обработка отправки формы form.login-form должна быть по событию submit.
+//     При отправке формы страница не должна перезагружаться.
+//     Если в форме есть незаполненные поля, выводи alert с предупреждением о том,
+//     что все поля должны быть заполнены.
+//     Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект,
+//     где имя поля будет именем свойства, а значение поля - значением свойства.
+//     Для доступа к элементам формы используй свойство elements.
+//     Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
+
+const form = document.querySelector(".login-form");
+const emailEl = document.querySelector("email");
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  //   const formData = new FormData(event.currentTarget);
+
+  //   formData.forEach((value, name) => {
+  //     console.log("submit name: ", name);
+  //     console.log("submit value", value);
+  //   });
+
+  //   console.log(formData.value);
+
+  const formDataElements = event.currentTarget.elements;
+  //   console.dir(formDataElements);
+  const email = formDataElements.email.value;
+  const pass = formDataElements.password.value;
+  //   console.log("email: ", email);
+  //   console.log("password: ", pass);
+
+  const dataForm = {
+    email,
+    pass,
+  };
+
+  if (email === "" || pass === "") {
+    alert("Error! All fields must be filled");
+  } else {
+    console.log(dataForm);
+  }
+  event.currentTarget.reset();
+}
